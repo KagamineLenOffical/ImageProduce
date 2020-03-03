bdde180f66bf69bf1cd776750595bd2aa9c64ca1
LiangJiSama <mingxiaoxi22@gmail.com>
2020-03-03 21:34:20 +0800

    new type of produce picture(3 color,bigger location point)

--- main.cpp
+++ main.cpp
@@ -2,25 +2,44 @@
 #include <opencv2/opencv.hpp>
 using namespace std;
 using namespace cv;
-const int ROW=1500;
-const int COL=2400;
-const int LEN=100;
-char a[1000]="Bernie Sanders has cemented his status as the Democratic front-runner to take on Donald Trump in November's US presidential election.He is projected to win Nevada's caucuses, and early results suggest he is on course for a large victory.There is a long way to go, however, until a nominee is confirmed.Early results also suggest former vice-president Joe Biden has performed better in Nevada than in the other two states which have voted so far.He had underwhelming results in Iowa and New Hampshire. Those states kicked off the four-month long primaries process, in which candidates are jostling to convince voters why they are the best candidate to challenge Mr Trump.";
+const int ROW=1000;
+const int COL=1000;
+const int LEN=20;
+int r=ROW/LEN,c=COL/LEN;
+char a[10000]="Bernie Sanders has cemented his status as the Democratic front-runner to take on Donald Trump in November's US presidential election.He is projected to win Nevada's caucuses, and early results suggest he is on course for a large victory.There is a long way to go, however, until a nominee is confirmed.Early results also suggest former vice-president Joe Biden has performed better in Nevada than in the other two states which have voted so far.He had underwhelming results in Iowa and New Hampshire. Those states kicked off the four-month long primaries process, in which candidates are jostling to convince voters why they are the best candidate to challenge Mr Trump.";
 //            Bernie Sanders has cemented his status as the Democratic front-runner to take on Donald Trump in November's US presidential election.He is projected to win Nevada's caucuses and early results suggest he is on course for a large victory.There is a long way to go, however, until a nominee is confirmed.Early results also suggest former vice-president Joe Biden has performed better in Nevada than in the other two states which have voted so far.He had underwhelming results in Iowa and New Hampshire. Those states kicked off the four-month long primaries process, in which candidates are jostling to convince voters why they are the best candidate to c
-bool src[10000];
+bool src[100000];
+int loc_point[10][10]={
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,1,1,1,1,1,1,1,0,0},
+        {0,1,0,0,0,0,0,1,0,0},
+        {0,1,0,1,1,1,0,1,0,0},
+        {0,1,0,1,1,1,0,1,0,0},
+        {0,1,0,1,1,1,0,1,0,0},
+        {0,1,0,0,0,0,0,1,0,0},
+        {0,1,1,1,1,1,1,1,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0}
+};
+int originpoint[ROW/LEN][COL/LEN];
+int srcpoint[ROW/LEN][COL/LEN];
 int contain,srclen;
 int image_produce();// 产生图片形式
 void video_produce(int piccnt);
+void srcpoint_produce();
+void src_produce();
 int main() {
-    int cnt=image_produce();
-
-    video_produce(cnt);
+//    int cnt=image_produce();
+//    video_produce(cnt);
+    src_produce();
+    srcpoint_produce();
+    video_produce(2);
     waitKey(0);
 }
 VideoWriter Vwriter;
 void video_produce(int piccnt){
-    int fps=30,perPicFps=5;
-    Vwriter=VideoWriter("dst.avi",VideoWriter::fourcc('M','J','P','G'),fps,Size(COL+2*LEN,ROW+2*LEN),0);
+    int fps=30,perPicFps=15;
+    Vwriter=VideoWriter("dst.avi",VideoWriter::fourcc('M','J','P','G'),fps,Size(COL,ROW),1);
     for(int i=0;i<piccnt;i++){
         if(!Vwriter.isOpened()){
             cout<<"failed to open writer";
@@ -29,12 +48,14 @@ void video_produce(int piccnt){
         char file_name[20];
         sprintf(file_name,"%d.png",i);
 
-//        printf("%s\n",file_name);
+        printf("%s\n",file_name);
 
-        Mat src=imread(file_name,CV_8U);
+        Mat src=imread(file_name,IMREAD_COLOR);
         //printf("%d %d===\n",Vwriter.)
         printf("%d %d\n",src.rows,src.cols);
-
+        namedWindow("src");
+        imshow("src",src);
+        waitKey(0);
 
         for(int j=1;j<=perPicFps;j++) {
             Vwriter << src;
@@ -42,18 +63,76 @@ void video_produce(int piccnt){
     }
 }
 
-int image_produce(){
-    Mat dst=Mat(ROW+2*LEN,COL+2*LEN,CV_8U);
+void srcpoint_produce(){
+    for(int i=0;i<c;i++)
+        for(int j=0;j<r;j++){
+            originpoint[i][j]=-1;
+        }
+    for(int i=0;i<10;i++)
+        for(int j=0;j<10;j++){
+            int k=loc_point[i][j]>0?7:0;
+            originpoint[i][j]=k;
+            originpoint[c-i-1][j]=k;
+            originpoint[i][r-j-1]=k;
+            originpoint[c-i-1][r-j-1]=k;
+        }
+    int cnt=0,piccnt=0;
+    printf("数据量（bit）：%d\n\n\n",srclen);
+    Mat dst=Mat(ROW,COL,CV_8UC3);
+
+    dst.setTo(255);
+    imwrite("0.png",dst);
+    while(cnt<srclen) {
+        piccnt++;
+        dst.setTo(255);
+        for (int i = 0; i < c; i++)
+            for (int j = 0; j < r; j++)srcpoint[i][j] = originpoint[i][j];
+
+        for(int i=0;i<c;i++)
+            for(int j=0;j<r;j++){
+                if(srcpoint[i][j]==-1){
+                    int k=(src[cnt]<<2)|(src[cnt+1]<<1)|(src[cnt+2]);
+                    cnt+=3;
+                    srcpoint[i][j]=k;
+                }
+            }
+        for(int i=0;i<c;i++)
+            for(int j=0;j<r;j++){
+                Scalar color;
+                for(int k=0;k<3;k++){
+                    if(((1<<k)&srcpoint[i][j])!=0)color.val[k]=0;
+                    else color.val[k]=255;
+
+                }
+                rectangle(dst, Point(i*LEN, j*LEN), Point(i*LEN + LEN, j*LEN + LEN), color, FILLED);
+            }
+        for (int i = 0; i < c; i++) {
+            for (int j = 0; j < r; j++) {
+                printf("%d", srcpoint[i][j]);
+            }
+            printf("\n");
+        }
+        char dst_file_name[20];
+        sprintf(dst_file_name,"%d.png",piccnt);
+        imwrite(dst_file_name,dst);
+    }
+}
+void src_produce(){
     int len=strlen(a);
     srclen=0;
     contain=ROW/LEN*(COL/LEN)-12;
     for(int i=0;i<len;i++){
         for(int j=0;j<8;j++){
             src[i*8+j]=(a[i]>>j)&1;
-            printf("%d",src[i*8+j]);
+            //printf("%d",src[i*8+j]);
             srclen++;
         }
-    }// 10000110 01001100 01100110 11001100 00100110
+    }
+}
+//this is the old version
+int image_produce(){
+    Mat dst=Mat(ROW+2*LEN,COL+2*LEN,CV_8U);
+
     dst.setTo(255);
     imwrite("0.png",dst);
     int cnt=0,piccnt=0;
