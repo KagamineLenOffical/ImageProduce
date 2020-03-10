f229ea0170a3722c19c9f0ed5a1c4b57dc74c9a9
LiangJiSama <mingxiaoxi22@gmail.com>
2020-03-10 17:57:29 +0800

    重写彩色码，加入彩色边框

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <cstdlib>
 using namespace std;
 using namespace cv;
 const int ROW=1000;
 const int COL=1000;
-const int LEN=20;
+const int LEN=10;
 int r=ROW/LEN,c=COL/LEN;
-char a[10000]="Bernie Sanders has cemented his status as the Democratic front-runner to take on Donald Trump in November's US presidential election.He is projected to win Nevada's caucuses, and early results suggest he is on course for a large victory.There is a long way to go, however, until a nominee is confirmed.Early results also suggest former vice-president Joe Biden has performed better in Nevada than in the other two states which have voted so far.He had underwhelming results in Iowa and New Hampshire. Those states kicked off the four-month long primaries process, in which candidates are jostling to convince voters why they are the best candidate to challenge Mr Trump.";
-//            Bernie Sanders has cemented his status as the Democratic front-runner to take on Donald Trump in November's US presidential election.He is projected to win Nevada's caucuses and early results suggest he is on course for a large victory.There is a long way to go, however, until a nominee is confirmed.Early results also suggest former vice-president Joe Biden has performed better in Nevada than in the other two states which have voted so far.He had underwhelming results in Iowa and New Hampshire. Those states kicked off the four-month long primaries process, in which candidates are jostling to convince voters why they are the best candidate to c
-bool src[100000];
+char a[100000];
+bool src[800000];
 int loc_point[10][10]={
         {0,0,0,0,0,0,0,0,0,0},
         {0,1,1,1,1,1,1,1,0,0},
@@ -26,19 +26,31 @@ int srcpoint[ROW/LEN][COL/LEN];
 int contain,srclen;
 int image_produce();// 产生图片形式
 void video_produce(int piccnt);
-void srcpoint_produce();
+int srcpoint_produce();
 void src_produce();
+void read_file();
 int main() {
 //    int cnt=image_produce();
 //    video_produce(cnt);
     src_produce();
-    srcpoint_produce();
-    video_produce(2);
+    int _piccnt=srcpoint_produce();
+    video_produce(_piccnt);
     waitKey(0);
+}
+void read_file(){
+    FILE *fin=fopen("1.in","r");
+    int len=0;
+    char ch=0;
+    while((ch=fgetc(fin))!=EOF){
+        a[len++]=ch;
+    }
+    printf("%d\n",len);
+    a[len]='\0';
+    return;
 }
 VideoWriter Vwriter;
 void video_produce(int piccnt){
-    int fps=30,perPicFps=15;
+    int fps=30,perPicFps=5;
     Vwriter=VideoWriter("dst.avi",VideoWriter::fourcc('M','J','P','G'),fps,Size(COL,ROW),1);
     for(int i=0;i<piccnt;i++){
         if(!Vwriter.isOpened()){
@@ -48,11 +60,11 @@ void video_produce(int piccnt){
         char file_name[20];
         sprintf(file_name,"%d.png",i);
 
-        printf("%s\n",file_name);
+        //printf("%s\n",file_name);
 
         Mat src=imread(file_name,IMREAD_COLOR);
         //printf("%d %d===\n",Vwriter.)
-        printf("%d %d\n",src.rows,src.cols);
+        //printf("%d %d\n",src.rows,src.cols);
         namedWindow("src");
         imshow("src",src);
         waitKey(0);
@@ -63,7 +75,7 @@ void video_produce(int piccnt){
     }
 }
 
-void srcpoint_produce(){
+int srcpoint_produce(){
     for(int i=0;i<c;i++)
         for(int j=0;j<r;j++){
             originpoint[i][j]=-1;
@@ -76,8 +88,14 @@ void srcpoint_produce(){
             originpoint[i][r-j-1]=k;
             originpoint[c-i-1][r-j-1]=k;
         }
+    for(int i=0;i<c;i++){
+        originpoint[i][0]=originpoint[i][r-1]=0;
+    }
+    for(int i=0;i<r;i++){
+        originpoint[0][i]=originpoint[c-1][i]=0;
+    }
     int cnt=0,piccnt=0;
-    printf("数据量（bit）：%d\n\n\n",srclen);
+    printf("数据量（bit）：%d\n",srclen);
     Mat dst=Mat(ROW,COL,CV_8UC3);
 
     dst.setTo(255);
@@ -106,28 +124,33 @@ void srcpoint_produce(){
                 }
                 rectangle(dst, Point(i*LEN, j*LEN), Point(i*LEN + LEN, j*LEN + LEN), color, FILLED);
             }
-        for (int i = 0; i < c; i++) {
+        /*for (int i = 0; i < c; i++) {
             for (int j = 0; j < r; j++) {
                 printf("%d", srcpoint[i][j]);
             }
             printf("\n");
-        }
+        }*/
         char dst_file_name[20];
         sprintf(dst_file_name,"%d.png",piccnt);
         imwrite(dst_file_name,dst);
     }
+    return piccnt;
 }
 void src_produce(){
+    read_file();
     int len=strlen(a);
     srclen=0;
     contain=ROW/LEN*(COL/LEN)-12;
+    FILE *fout=fopen("2.out","w+");
     for(int i=0;i<len;i++){
         for(int j=0;j<8;j++){
             src[i*8+j]=(a[i]>>j)&1;
-            //printf("%d",src[i*8+j]);
+            fprintf(fout,"%d",src[i*8+j]);
             srclen++;
         }
     }
+    fclose(fout);
+    //printf("%d\n",srclen);
 }
 //this is the old version
 int image_produce(){
