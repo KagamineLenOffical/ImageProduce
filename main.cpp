79f68d681b21489d5d36b92b2bbd4f9eeb953626
TerwalEast <61215802+TerwalEast@users.noreply.github.com>
2020-03-28 22:53:36 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 #define Buffer_Size_By_Byte 1024*1024*10
 #define Buffer_Size_By_Bit 1024*1024*10*8
+#define Data_Block_Size_By_Bit 256      //改变这个可以调节数据块大小。具体多大最合适和错误率挂钩，我没算。暂时不要改动。否则会出错。
 
 using namespace std;
 using namespace cv;
@@ -12,20 +13,21 @@ const int COL=1800;
 const int LEN=10;
 int r=ROW/LEN,c=COL/LEN;
 unsigned char Raw_Data_Input_Buffer[Buffer_Size_By_Byte];
-bool Data_Buffer_By_Bit[Buffer_Size_By_Bit];
+bool Raw_Data_Buffer_By_Bit[Buffer_Size_By_Bit];
+bool Processed_Data_Buffer_By_Bit[Buffer_Size_By_Bit];
 int loc_point[10][10]=
-        {
-                {0,0,0,0,0,0,0,0,0,0},
-                {0,1,1,1,1,1,1,1,0,0},
-                {0,1,0,0,0,0,0,1,0,0},
-                {0,1,0,1,1,1,0,1,0,0},
-                {0,1,0,1,1,1,0,1,0,0},
-                {0,1,0,1,1,1,0,1,0,0},
-                {0,1,0,0,0,0,0,1,0,0},
-                {0,1,1,1,1,1,1,1,0,0},
-                {0,0,0,0,0,0,0,0,0,0},
-                {0,0,0,0,0,0,0,0,0,0}
-        };
+{
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,1,1,1,1,1,1,1,0,0},
+        {0,1,0,0,0,0,0,1,0,0},
+        {0,1,0,1,1,1,0,1,0,0},
+        {0,1,0,1,1,1,0,1,0,0},
+        {0,1,0,1,1,1,0,1,0,0},
+        {0,1,0,0,0,0,0,1,0,0},
+        {0,1,1,1,1,1,1,1,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0}
+};
 
 int originpoint[COL/LEN][ROW/LEN];
 int srcpoint[COL/LEN][ROW/LEN];
@@ -47,15 +49,15 @@ long Zipped_Data_Length_By_Byte;
 int main(int argc,char *argv[]) {
 //    int cnt=image_produce();
 //    video_produce(cnt);
-
+    
     Video_Time_Buffer_By_MS = atoi(argv[3]);
     strcpy(Input_Path_Buffer, argv[1]);
     strcpy(Output_Path_Buffer, argv[2]);
+    
+    
+    
 
-
-
-
-
+    
     src_produce();
 
     int _piccnt=srcpoint_produce();
@@ -82,7 +84,7 @@ void read_file()
         Raw_Data_Length_By_Byte+=fread(Raw_Data_Input_Buffer+i, 1, 1024, fin);
         i+=1024;
     }
-
+    
     printf("总计读取到%d字节\n",Raw_Data_Length_By_Byte);
 }
 
@@ -96,7 +98,7 @@ void video_produce(int piccnt)
     float Video_Length_Tracker = 0;
     float Time_Per_Frame_By_Ms = 1000*(1.0/(float)fps);
     Vwriter=VideoWriter(Output_Path_Buffer,VideoWriter::fourcc('M','J','P','G'),fps,Size(COL,ROW),1);
-    for(int i=0;i<=piccnt;i++)
+    for(int i=0;i<piccnt;i++)
     {
         if(!Vwriter.isOpened())
         {
@@ -125,8 +127,8 @@ void video_produce(int piccnt)
             }
         }
     }
-    Break_The_Loop:
-    cout << "/nFinished!" << endl;
+Break_The_Loop:
+    cout << endl <<"Finished!" << endl;
 }
 
 
@@ -158,7 +160,8 @@ int srcpoint_produce()
     }
     int cnt=0;
     int piccnt=0;
-    printf("数据量（bit）：%d\n",srclen);
+
+    printf("处理后数据量（bit）：%d\n",srclen);
     Mat dst=Mat(ROW,COL,CV_8UC3);
 
     dst.setTo(255);
@@ -174,14 +177,14 @@ int srcpoint_produce()
                 srcpoint[i][j] = originpoint[i][j];
             }
         }
-
+        
         for(int i=0;i<c;i++)
         {
             for(int j=0;j<r;j++)
             {
                 if(srcpoint[i][j]==-1)
                 {
-                    int k=(Data_Buffer_By_Bit[cnt]<<2)|(Data_Buffer_By_Bit[cnt+1]<<1)|(Data_Buffer_By_Bit[cnt+2]);
+                    int k=(Processed_Data_Buffer_By_Bit[cnt]<<2)|(Processed_Data_Buffer_By_Bit[cnt+1]<<1)|(Processed_Data_Buffer_By_Bit[cnt+2]);
                     cnt+=3;
                     srcpoint[i][j]=k;
                 }
@@ -201,12 +204,9 @@ int srcpoint_produce()
                 rectangle(dst, Point(i*LEN, j*LEN), Point(i*LEN + LEN, j*LEN + LEN), color, FILLED);
             }
         }
-        /*for (int i = 0; i < c; i++) {
-            for (int j = 0; j < r; j++) {
-                printf("%d", srcpoint[i][j]);
-            }
-            printf("\n");
-        }*/
+        
+ 
+        
         char dst_file_name[20];
         sprintf(dst_file_name,"%d.png",piccnt);
         imwrite(dst_file_name,dst);
@@ -219,61 +219,85 @@ int srcpoint_produce()
 
 void src_produce()
 {
+    int Parity_Current_Parity_Tracker = 0;    //采用奇校验。亦即算上校验位本身有奇数个1.
+    int Parity_Number_Tracker = 0;
+    int Processed_Buffer_Marker = 0;
+    
     read_file();
 
+    
+    
 
-
-
-
+   
     srclen=0;
     contain=ROW/LEN*(COL/LEN)-12;
-    FILE *fout=fopen("2.out","w+");
+    FILE *fout=fopen("Raw.out","w+");
+    
     for(int i = 0;i < Raw_Data_Length_By_Byte;i++)
     {
         for(int j = 0;j < 8;j++)
         {
-            Data_Buffer_By_Bit[i*8+j]=(Raw_Data_Input_Buffer[i]>>j)&1;
-            fprintf(fout,"%d",Data_Buffer_By_Bit[i*8+j]);
+            Raw_Data_Buffer_By_Bit[i*8+j]=(Raw_Data_Input_Buffer[i]>>j)&1;
+            fprintf(fout,"%d",Raw_Data_Buffer_By_Bit[i*8+j]);
             srclen++;
         }
     }
+    printf("处理后数据量（bit）：%d\n",srclen);
+    srclen = 0;
     fclose(fout);
-    //printf("%d\n",srclen);
+    /*----------------------MARK，出错找这里--------------------------*/
+    
+    
+    fout=fopen("Processed.out","w+");
+    
+    
+    for(int i = 0;i < Raw_Data_Length_By_Byte;i++)
+    {
+        for(int j = 0;j < 8;j++)
+        {
+            Processed_Data_Buffer_By_Bit[Processed_Buffer_Marker]=(Raw_Data_Input_Buffer[i]>>j)&1;
+            
+            fprintf(fout,"%d",Processed_Data_Buffer_By_Bit[Processed_Buffer_Marker]);
+            srclen++;
+            
+            
+            if( Processed_Data_Buffer_By_Bit[Processed_Buffer_Marker] )
+            {
+                Parity_Current_Parity_Tracker ++;
+            }
+            
+            
+            Processed_Buffer_Marker ++;
+            Parity_Number_Tracker ++ ;
+
+            
+            
+            if( Parity_Number_Tracker == Data_Block_Size_By_Bit)
+            {
+                if( Parity_Current_Parity_Tracker % 2)
+                {
+                    fputc('0', fout);
+                    Processed_Data_Buffer_By_Bit[Processed_Buffer_Marker] = 0;
+                    Processed_Buffer_Marker ++;
+                    srclen++;
+                }
+                else
+                {
+                    fputc('1', fout);
+                    Processed_Data_Buffer_By_Bit[Processed_Buffer_Marker] = 1;
+                    Processed_Buffer_Marker ++;
+                    srclen++;
+                }
+                Parity_Current_Parity_Tracker = 0;
+                Parity_Number_Tracker = 0;
+                
+            }
+            
+        }
+    }
+    fclose(fout);
+
+    printf("校验添加后为%d比特\n",srclen);
 }
 
 
-
-
-
-
-
-//this is the old version
-//int image_produce(){
-//    Mat dst=Mat(ROW+2*LEN,COL+2*LEN,CV_8U);
-//
-//    dst.setTo(255);
-//    imwrite("0.png",dst);
-//    int cnt=0,piccnt=0;
-//    while(cnt<srclen) {
-//        dst.setTo(255);
-//        for (int i = 0; i < COL / LEN; i++)
-//            for (int j = 0; j < ROW / LEN; j++) {
-//                int x = (i + 1) * LEN, y = (j + 1) * LEN;
-//                if ((i == 0 && j == 0) || (i == COL / LEN - 1 && j == 0) || (i == 0 && j == ROW / LEN - 1)) {
-//                    int p = LEN / 5;
-//                    rectangle(dst, Point(x, y), Point(x + LEN, y + LEN), Scalar(0, 0, 0), FILLED);
-//                    rectangle(dst, Point(x + p, y + p), Point(x + LEN - p, y + LEN - p), Scalar(255, 255, 255), FILLED);
-//                    rectangle(dst, Point(x + 2 * p, y + 2 * p), Point(x + 3 * p, y + 3 * p), Scalar(0, 0, 0), FILLED);
-//                } else if ((i == 0 && (j == 1 || j == ROW / LEN - 2)) ||
-//                           (i == 1 && (j == 0 || j == 1 || j == ROW / LEN - 2 || j == ROW / LEN - 1)) ||
-//                           (i == COL / LEN - 2 && (j == 0 || j == 1)) || (i == COL / LEN - 1 && j == 1)) { continue; }
-//                else if (Source_Data_Buffer_By_Bit[cnt++] == 1)rectangle(dst, Point(x, y), Point(x + LEN, y + LEN), Scalar(0, 0, 0), FILLED);
-//            }
-//        piccnt++;
-//        char dst_file_name[20];
-//        sprintf(dst_file_name,"%d.png",piccnt);
-//        imwrite(dst_file_name,dst);
-//    }
-//
-//    return piccnt;
-//}
